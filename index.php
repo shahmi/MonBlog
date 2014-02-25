0fb6d497825b2da32a54b0e4e11163169975846c
Mickael <Mickael@134.214.119.245>
2014-02-25 16:56:16 +0100

    Fail dans le précèdent ajout.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8" />
         <link rel="stylesheet" href="style.css" />
-        <title><a href="index.php"><h1 id="titreBlog">Mon Blog</h1></a></title>
+        <title>Mon Blog</title>
     </head>
     <body>
         <div id="global">
